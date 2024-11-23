7bd59f6911b9762094fd4b77ca3888c566cfb5a4
Victor Momodu <victoramomodu@gmail.com>
2024-11-23 13:46:17 +0100

    feat: Restructure to use one prisma instance.

--- luxyRide-backend/apps/customers/src/auth/auth.module.ts
+++ luxyRide-backend/apps/customers/src/auth/auth.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaModule } from '../prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 import { MessagingModule } from 'library/messaging';
 
 @Module({
-  imports: [PrismaModule, JwtModule, MessagingModule],
+  imports: [JwtModule, MessagingModule],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
--- luxyRide-backend/apps/customers/src/customers.module.ts
+++ luxyRide-backend/apps/customers/src/customers.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
+import { DatabaseModule } from 'library/database';
 
 @Module({
   imports: [
     AuthModule,
-    PrismaModule,
+    DatabaseModule,
     ConfigModule.forRoot({ isGlobal: true }),
     CacheModule.register({ isGlobal: true }),
   ],
